8c7c071f042f35f042415fc2d1492512cfd21377
Raelon Veritas Lee <roclee2692@gmail.com>
2025-07-05 20:20:34 +0800

    refactor: centralize constant definitions

---
+++ common/constants.js
@@ -0,0 +1,12 @@
+module.exports = {
+  ADMIN_OPENIDS: ['admin_openid'],
+  LOG_STATUS: {
+    PENDING: 'pending',
+    APPROVED: 'approved',
+  },
+  LOG_TYPES: {
+    LABOR: 'labor',
+    VOLUNTEER: 'volunteer',
+  },
+  VOLUNTEER_POINTS_MULTIPLIER: 2,
+};
--- miniprogram/common/constants.js
+++ miniprogram/common/constants.js
@@ -1,12 +1,1 @@
-module.exports = {
-  ADMIN_OPENIDS: ['admin_openid'],
-  LOG_STATUS: {
-    PENDING: 'pending',
-    APPROVED: 'approved',
-  },
-  LOG_TYPES: {
-    LABOR: 'labor',
-    VOLUNTEER: 'volunteer',
-  },
-  VOLUNTEER_POINTS_MULTIPLIER: 2,
-};
+module.exports = require('../../common/constants');
