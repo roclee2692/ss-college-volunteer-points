dd3f15ff850aa3628068a6403c7e6769417cdae8
Raelon Veritas Lee <roclee2692@gmail.com>
2025-07-05 14:48:18 +0800

    test: restore globals around Page stub

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,6 +3,17 @@ const path = require('path');
 const simulate = require('miniprogram-simulate');
 
 test('index page navigation', () => {
+  const originalPage = global.Page;
+  let def;
+  global.Page = (obj) => { def = obj; };
+  // eslint-disable-next-line global-require
+  require('../miniprogram/pages/index/index');
+
+  global.Page = () => {};
+  // eslint-disable-next-line global-require
+  require('../miniprogram/pages/home/home');
+
+  const originalNavigate = wx.navigateTo;
   wx.navigateTo = jest.fn();
 
   const template = fs.readFileSync(
@@ -13,5 +24,9 @@ test('index page navigation', () => {
   const page = simulate.render(id);
   page.attach(document.createElement('parent'));
   page.instance.handleStart();
+
   expect(wx.navigateTo).toHaveBeenCalledWith({ url: '/pages/home/home' });
+
+  wx.navigateTo = originalNavigate;
+  global.Page = originalPage;
 });
