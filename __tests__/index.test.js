9dfb845283825d3c38a15fdcaf1de7b5c03457c5
Raelon Veritas Lee <roclee2692@gmail.com>
2025-07-05 13:55:55 +0800

    Refactor test initialization

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 const path = require('path');
 const simulate = require('miniprogram-simulate');
 
+let def;
+global.Page = (obj) => { def = obj; };
+require('../miniprogram/pages/index/index');
+
 test('index page navigation', () => {
-  let def;
-  global.Page = (obj) => { def = obj; };
-  require('../miniprogram/pages/index/index.js');
   wx.navigateTo = jest.fn();
 
   const template = fs.readFileSync(
     path.resolve(__dirname, '../miniprogram/pages/index/index.wxml'),
-    'utf8'
+    'utf8',
   );
   const id = simulate.load({ template, methods: { handleStart: def.handleStart } });
   const page = simulate.render(id);
