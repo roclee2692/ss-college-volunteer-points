f950affd591d69250633fb8de86b5a6d28bf92b1
Raelon Veritas Lee <roclee2692@gmail.com>
2025-07-05 14:03:42 +0800

    fix tests lint

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -5,12 +5,13 @@ const simulate = require('miniprogram-simulate');
 test('index page navigation', () => {
   let def;
   global.Page = (obj) => { def = obj; };
-  require('../miniprogram/pages/index/index.js');
+  // eslint-disable-next-line global-require
+  require('../miniprogram/pages/index/index');
   wx.navigateTo = jest.fn();
 
   const template = fs.readFileSync(
     path.resolve(__dirname, '../miniprogram/pages/index/index.wxml'),
-    'utf8'
+    'utf8',
   );
   const id = simulate.load({ template, methods: { handleStart: def.handleStart } });
   const page = simulate.render(id);
